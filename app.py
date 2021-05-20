b47309de73b496c8f213df31ca5e9b48d7b71f5c
dragonflypie <canuczerealme@gmail.com>
2021-05-21 01:19:50 +0300

    basic routes start db

--- app.py
+++ app.py
@@ -3,10 +3,54 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plans.db"
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+class goal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    type = db.Column(db.String(10), nullable=False, default="ToDo")
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    deadline = db.Column(db.DateTime)
+    time = db.Column(db.DateTime)
+    duration = db.Column(db.DateTime)
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__== "__main__":
+
+@app.route("register")
+def register():
+    return "apology"
+
+
+@app.route("login")
+def login():
+    return "apology"
+
+
+@app.route("new")
+def new():
+    return ""
+
+
+@app.route("lookup")
+def lookup():
+    pass
+
+
+@app.route("settings")
+def settings():
+    pass
+
+
+@app.route("apology")
+def apology():
+    pass
+
+
+if __name__ == "__main__":
     app.run(debug=True)
