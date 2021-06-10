b0c13d521f9a8b906ec1d5749f772130ce8ef006
dragonflypie <canuczerealme@gmail.com>
2021-06-10 13:15:22 +0300

    80% style

--- static/new.js
+++ static/new.js
@@ -1,17 +1,21 @@
 let weekdays = () => {
   let week = document.getElementById("week_checkbox");
+  let datepicker = document.getElementById("datepickr");
   let checkbox = document.querySelector("#radioweeek");
   checkbox.addEventListener("change", function (e) {
     let target = e.target;
     switch (target.id) {
       case "radioweek-0":
         week.style.display = "none";
+        datepicker.style.display = "block";
         break;
       case "radioweek-1":
-        week.style.display = "block";
+        week.style.display = "flex";
+        datepicker.style.display = "none";
         break;
       case "radioweek-2":
         week.style.display = "none";
+        datepicker.style.display = "block";
         break;
     }
   });
